4bfc9cc46a605caaf97790f7e8e91a263c1af421
whd <18404983786@163.com>
2019-09-09 21:10:16 +0800

    "安装完成Redis之后测试RedisTemplate"

--- src/main/java/com/whd/initialize/controller/TestController.java
+++ src/main/java/com/whd/initialize/controller/TestController.java
@@ -2,9 +2,12 @@ package com.whd.initialize.controller;
 
 import com.whd.initialize.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author WHD
@@ -17,10 +20,20 @@ public class TestController {
     @Autowired
     private TestService testService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @RequestMapping("/getAll")
     @ResponseBody
     public Object getAll(){
         return testService.getAll();
+    }
+
+    @RequestMapping("/testRedis")
+    @ResponseBody
+    public Object testRedis(){
+    	redisTemplate.opsForValue().set("test","test",300, TimeUnit.SECONDS);
+        return "success";
     }
 
 }
